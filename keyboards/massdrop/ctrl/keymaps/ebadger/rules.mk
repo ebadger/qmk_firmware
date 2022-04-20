3b1cc8b208d1e8501da4329c1f4a7524a83dcf84
Eric Badger <ebadger@microsoft.com>
2022-04-20 10:48:38 -0700

    push keymaps :

---
+++ keyboards/massdrop/ctrl/keymaps/ebadger/rules.mk
@@ -0,0 +1,7 @@
+SRC += hid_lamparray.c
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+#RAW_ENABLE = yes             # Raw devicen
+#EXTRAKEY_ENABLE = no
+#NKRO_ENABLE = no
+HID_LAMPARRAY_ENABLE = yes
